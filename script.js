34e78c1bd88bba377dbdf915546132db58f31bc7
hardrock28 <50201375+hardrock28@users.noreply.github.com>
2023-02-11 18:17:13 +0530

    new commit

--- script.js
+++ script.js
@@ -4,22 +4,49 @@ let arr = [
   { id: 3, name: "karen", age: "19", marks: 35 },
 ];
 
-function PrintStudentswithMap() {
+function PrintStudentbyMap() {
   //Write your code here , just console.log
+  arr.map((arr) => {
+    if (arr.marks > 50) {
+      console.log(arr.id + " " + arr.name + " " + arr.age + " " + arr.marks);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((arr) => {
+    if (arr.marks > 50) {
+      console.log(arr.id + " " + arr.name + " " + arr.age + " " + arr.marks);
+    }
+  });
 }
 
-function addData() {
+function addData(id, name, age, marks) {
   //Write your code here, just console.log
+  let newData = {
+    id: 4,
+    name: "Parab",
+    age: "26",
+    marks: 90,
+  };
+  arr.push(newData);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter((arr) => arr.marks >= 50);
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArr = [
+    { id: 4, name: "Harris", age: "15", marks: 50 },
+    { id: 5, name: "John", age: "23", marks: 75 },
+    { id: 6, name: "Morris", age: "41", marks: 65 },
+  ];
+  let concatenate = arr.concat(newArr);
+  console.log(concatenate);
 }
